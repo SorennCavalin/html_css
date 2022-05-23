42d3713bd2fc323260dfdbd831cb1d2139ae030e
Sorenn <sorenn.cavalin@lepoles.org>
2022-05-23 16:55:34 +0200

    fin-jour-35

--- exo.js
+++ exo.js
@@ -1,61 +1,22 @@
-// //alert("Hi");
-
-// /*---------
-// LA CONCATENATION
-// ---------*/
-// var annee = 2017; // à quoi sert une var, différents types de var
-// var futur = 3;
-// var calcul = annee + futur;
-// console.log(calcul);
-// document.write(calcul + "&nbsp,&nbsp" + " " + " " + calcul + "<br>"); // -> 2020 avec saut de ligne
-// //document.write(" ");// -> 2020 avec saut de ligne
-// //document.write(calcul + "<br>");// -> 2020 avec saut de ligne
-// document.write(calcul + "<br>"); // -> 2020 avec saut de ligne
-
-// var mois = "7";
-// var calcul2 = annee + mois;
-// console.log(calcul2); // -> 20207<br>  parce que pas de calcul avec des strings
-// document.write(calcul2 + "<br>");
-
-// var debutPhrase = "Aujourd'hui ";
-// var nbStagiaires = 12;
-// var suitePhrase = " stagiaires";
-// var finPhrase = "sont présents."; // pas d'espace exprès
-
-// // -- Nous souhaitons afficher la phrase en un seul morceau avec la concaténation => à vos claviers !
-// document.write(debutPhrase + nbStagiaires + suitePhrase + " " + finPhrase);
-// // le " " est utile quand on a des données récupérées directement de l'utilisateur
+// var fruit = [];
 
 
 
-// var nb1 = 1;
-// var nb2 = 5;
-// var res = nb1 + nb2;
-// document.write("Le résultat est " + res)
+// for (var i = 0; i < 5; i++) {
 
-// var nb1 = 25;
-// var nb2 = 3;
-// var res = nb1 + nb2;
-// console.log(res);
-// document.write(" <br> le resultat est " + res + "<br> saute une ligne <br>")
+//   fruit[i] = prompt("tapez un fruit");
 
-// var finPhrase = "stagiaire";
-// var suitePhrase = "sont présents";
-// console.log(typeof finPhrase, typeof suitePhrase);
+//   document.write("fruit tapé : " + fruit[i] + "<br>");
 
-var premier = Number(prompt(""))
-var second = Number(prompt(""))
-var trois = Number(prompt(""))
+// }
 
-if (premier > second) {
-    premier = trois * 3
-} else  if (trois < second) {
-        premier++
-        if (premier === trois) {
-            second = (premier + trois) * 3
-        }
+var saisie = prompt("");
+var number = Number(saisie)
+if (isNaN(number)) {
+    document.write("vous avez tapé un string : " + saisie);
 } else {
-    alert("je suis perdu")
+    document.write("vous avez tapé un nombre : " + number);
 }
-document.write(premier + "<br>" + second + "<br>" + trois )
+;
+
 
