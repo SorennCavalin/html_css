6ebd58fb1039d4f1bc92087fe7abe1dfd00870e7
Sorenn <sorenn.cavalin@lepoles.org>
2022-06-14 16:49:40 +0200

    14/06/2022

---
+++ JqueryUI/assets/js/app.js
@@ -0,0 +1,7 @@
+$(function () {
+    var slideImg = ["https://cdn.pixabay.com/photo/2016/04/15/10/35/horse-1330690_960_720.jpg", "https://cdn.pixabay.com/photo/2018/05/11/11/11/horse-3390256_960_720.jpg", "https://cdn.pixabay.com/photo/2018/05/13/01/39/horse-3395135_960_720.jpg", "https://cdn.pixabay.com/photo/2018/01/22/14/13/animal-3099035_960_720.jpg", "https://cdn.pixabay.com/photo/2018/06/11/00/21/foal-3467629_960_720.jpg"];
+
+    const slider = new MySlider($('#simple-slider'),slideImg)
+})
+
+
